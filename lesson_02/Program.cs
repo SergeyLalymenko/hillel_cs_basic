b2059bd74b03fa375b05721f8910825a58402b06
SergeyLalymenko <Lalymenko.Sergey153@gmail.com>
2025-01-22 21:05:29 +0200

    update

--- lesson_02/Program.cs
+++ lesson_02/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string client = "";
+string product = "";
+string address = "";
+
+for (int i = 1; i < 4; i++)
+{
+    Console.WriteLine("Enter client name: ");
+    client = Console.ReadLine();
+    Console.WriteLine("Enter product: ");
+    product = Console.ReadLine();
+    Console.WriteLine("Enter address: ");
+    address = Console.ReadLine();
+
+    Console.WriteLine($"Order No {i}\nClient: {client}.\nProduct: {product}.\nAddress: {address}.");
+}
