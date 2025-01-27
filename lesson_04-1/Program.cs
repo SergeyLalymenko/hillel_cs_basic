14ad1023497be92ea204da584663de617cd8afde
SergeyLalymenko <Lalymenko.Sergey153@gmail.com>
2025-01-27 21:08:50 +0200

    hw4

---
+++ lesson_04-1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter number 1 - 7:");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+string[] daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+Console.WriteLine(daysOfWeek[number - 1]);
+
+
---
+++ lesson_04-2/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Enter number 1:");
+string input1 = Console.ReadLine();
+Console.WriteLine("Enter number 2:");
+string input2 = Console.ReadLine();
+
+int number1 = Convert.ToInt32(input1);
+int number2 = Convert.ToInt32(input2);
+
+Console.WriteLine("Enter operation:");
+string operation = Console.ReadLine();
+
+float result = 0;
+
+switch (operation)
+{
+    case "*":
+        {
+            result = number1 * number2;
+            break;
+        }
+    case "/":
+        {
+            result = (float)number1 / number2;
+            break;
+        }
+    case "+":
+        {
+            result = number1 + number2;
+            break;
+        }
+    case "-":
+        {
+            result = number1 - number2;
+            break;
+        }
+}
+
+Console.WriteLine($"Result: {result}");
