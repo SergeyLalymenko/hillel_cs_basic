95c28aa631711d40b3c68be701f1326afc5de77f
SergeyLalymenko <Lalymenko.Sergey153@gmail.com>
2025-02-02 17:33:17 +0200

    update

--- lesson_04-1/Program.cs
+++ lesson_04-1/Program.cs
@@ -2,6 +2,7 @@
 string input = Console.ReadLine();
 int number = Convert.ToInt32(input);
 string[] daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-Console.WriteLine(daysOfWeek[number - 1]);
+string result = daysOfWeek[number - 1] ? daysOfWeek[number - 1] : "Invalid";
+Console.WriteLine(result);
 
 
---
+++ lesson_05-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Enter number 1:");
+string input1 = Console.ReadLine();
+Console.WriteLine("Enter number 2:");
+string input2 = Console.ReadLine();
+float number1 = 0;
+float number2 = 0;
+
+if(float.TryParse(input1, out number1) && float.TryParse(input2, out number2))
+{
+    if (number2 == 0)
+    {
+        Console.WriteLine("Divide by zero");
+    }
+    else
+    {
+        Console.WriteLine($"Result: {number1 / number2}");
+    }
+}
+else
+{
+    Console.WriteLine("Incorrect input");
+}
